43093ecc7368118edbb03a59797e11ea0fe92e78
Odilbek <odilbekjuraev07@gmail.com>
2023-02-08 13:20:57 +0500

    some changes done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:local_notifications/data/notification_service/local_notification_service.dart';
+
+import 'ui/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // LocalNotificationService.localNotificationService.init();
   runApp(const MyApp());
 }
 
@@ -10,11 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Local Notifications',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:local_notifications/data/notification_service/local_notification_service.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentId = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                currentId++;
+                LocalNotificationService.localNotificationService
+                    .showNotification(id: currentId);
+              },
+              child: Text("SIMPLE Notifiction 1"),
+            ),
+            TextButton(
+              onPressed: () {
+                currentId++;
+                LocalNotificationService.localNotificationService
+                    .showNotification(id: currentId);
+              },
+              child: Text("SIMPLE Notifiction 2"),
+            ),
+            TextButton(
+              onPressed: () {
+                currentId++;
+                LocalNotificationService.localNotificationService
+                    .scheduleNotification(
+                  id: currentId,
+                  delayedTime: 3,
+                );
+              },
+              child: Text("SCHUADULED NOTIFICATION "),
+            ),
+            TextButton(
+              onPressed: () {
+                currentId++;
+                LocalNotificationService.localNotificationService
+                    .showPeriodically(id: currentId);
+              },
+              child: Text("PERIODIC NOTIFICATION EVERY MINUT"),
+            ),
+            const Expanded(child: SizedBox()),
+            TextButton(
+                onPressed: () {
+                  LocalNotificationService.localNotificationService
+                      .cancelAllNotifications();
+                },
+                child: const Text("Cancel All Notifications")),
+            TextButton(
+                onPressed: () {
+                  LocalNotificationService.localNotificationService
+                      .cancelNotificationById(currentId);
+                },
+                child: const Text("Cancel Notification By id")),
+          ],
+        ),
+      ),
+    );
+  }
+}
