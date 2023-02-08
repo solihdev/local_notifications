197a78fb4faa9784a25b1b4b72295557f710e57d
Odilbek <odilbekjuraev07@gmail.com>
2023-02-08 13:41:09 +0500

    some changes done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:local_notifications/data/notification_service/local_notification_service.dart';
 
 import 'ui/home_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  // LocalNotificationService.localNotificationService.init();
+  // LocalNotificationService.localNotificationService.init( );
   runApp(const MyApp());
 }
 
